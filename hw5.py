6ff51b3933e4d69464f7e1dde0472f22a237f7af
Christian Hart <christiantaylorhart@gmail.com>
2022-09-29 17:55:55 -0400

    initial commit

---
+++ hw5.py
@@ -0,0 +1,12 @@
+
+    # Write a function that takes a string as input and does the following:
+
+    # Returns True if the input string has more vowels than consonants
+    # Returns False if the input string has more consonants than vowels
+    # Returns None (this is the Python equivalent of null) if the input string has an equal number of consonants and vowels. We’ll ignore type safety for now!
+
+    # The volume of a sphere is given by the formula V = (4/3)πr^3. Given an input R, return the volume of a sphere with radius R. You can assume R is positive.
+
+    # Comma-separated values (CSV) is a popular format for storing data. For the first step of the CSV portion of this assignment, write a function that takes a list of strings as inputs, and returns a single string created by joining all the input strings together, with a comma separating them. Example: ["I", "Love", "Python"] -> "I,Love,Python"
+
+    # Now write another function that takes two arguments: a list of lists of strings, and a filepath. The function should apply the operation from question 3 to each list in the list, and write the result as a row of the file at filepath. The function doesn't need to return anything.
